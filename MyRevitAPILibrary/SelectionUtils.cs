f91eeeed6361c9e54430a67738cbcde6bd8eeefd
BelikAE <BelikAE@HOME-PC>
2025-02-23 14:14:40 +1000

    ds

---
+++ MyRevitAPILibrary/SelectionUtils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB;
+
+namespace MyRevitAPILibrary
+{
+    public class SelectionUtils
+    {
+        public static List<Pipe> SelectionPipes(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pipes = new FilteredElementCollector(doc)
+                .OfClass(typeof(Pipe))
+                .Cast<Pipe>()
+                .ToList();
+
+            return pipes;
+        }
+
+        public static List<Wall> SelectionWalls(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>()
+            .ToList();
+            return walls;
+        }
+
+        public static List<Element> SelectionWindows(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Element> windows = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .WhereElementIsNotElementType()
+                .Cast<Element>()
+            .ToList();
+            return windows;
+        }
+
+        public static List<Element> SelectionDoors(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Element> doors = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .WhereElementIsNotElementType()
+                .Cast<Element>()
+                .ToList();
+
+            return doors;
+        }
+    }
+}
